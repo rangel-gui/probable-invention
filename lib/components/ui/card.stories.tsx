065468a2fb16251ae41bf7e9c247223f4b9136bc
Gui Rangel <guirangel.310@gmail.com>
2025-01-09 14:51:27 -0300

    Add card component and story

---
+++ lib/components/ui/card.stories.tsx
@@ -0,0 +1,49 @@
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+import { ComponentProps } from "react";
+
+type CardPropsWithCustomArgs = ComponentProps<typeof Card> & {
+  title: string;
+  description?: string;
+  content?: string;
+  footer?: string;
+};
+
+const meta: Meta<CardPropsWithCustomArgs> = {
+  //TODO: why is this giving a type error?
+  component: Card,
+  tags: ["autodocs"],
+  args: {
+    title: "Card Title",
+    description: "Card Description",
+    content: "Card Content",
+    footer: "Card Footer",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Card>
+      <CardHeader>
+        <CardTitle>{args.title}</CardTitle>
+        <CardDescription>{args.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{args.content}</p>
+      </CardContent>
+      <CardFooter>
+        <p>{args.footer}</p>
+      </CardFooter>
+    </Card>
+  ),
+};
