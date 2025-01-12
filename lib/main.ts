78ec7bf7e45cdd3e7e2ed9e1d073ce3f0d5bb2b4
Gui Rangel <guirangel.310@gmail.com>
2025-01-12 13:36:26 -0300

    Export input, label and card components

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,6 @@
+import "./index.css";
 export { Button, buttonVariants } from "./components/ui/button";
 export type { ButtonProps } from "./components/ui/button";
-import "./index.css";
+export { Input } from "./components/ui/input";
+export { Label } from "./components/ui/label";
+export { Card } from "./components/ui/card";
