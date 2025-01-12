ed434a33dc252a0158bc775310c05cc955114f86
Gui Rangel <guirangel.310@gmail.com>
2025-01-12 09:58:12 -0300

    Add label component

---
+++ lib/components/ui/label.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Label } from "./label";
+import { expect, userEvent, within } from "@storybook/test";
+
+const meta: Meta<typeof Label> = {
+  component: Label,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "Label",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    children: "Label",
+  },
+  render: (args) => (
+    <div className="flex items-center space-x-2">
+      <input type="checkbox" className="peer" id="terms" disabled />
+      <Label {...args} htmlFor="terms" />
+    </div>
+  ),
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const label = canvas.getByLabelText(args.children?.toString()!);
+    const checkbox = canvas.getByRole("checkbox");
+
+    await userEvent.click(label);
+
+    await expect(checkbox).not.toBeChecked();
+  },
+};
