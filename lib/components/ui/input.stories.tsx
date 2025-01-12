18be630c06b3bdf9ba20ce61f987abb29bf37738
Gui Rangel <guirangel.310@gmail.com>
2025-01-12 10:07:14 -0300

    Add input component

---
+++ lib/components/ui/input.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Input } from "./input";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+  tags: ["autodocs"],
+  args: {
+    placeholder: "Placeholder",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const File: Story = {
+  args: {
+    type: "file",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
