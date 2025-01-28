7ebec106412942df2db325224fbaa3e76d1daec2
Gui Rangel <guirangel.310@gmail.com>
2025-01-28 16:12:17 -0300

    Add checkbox component

---
+++ lib/components/ui/checkbox.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "./checkbox";
+import { Label } from "./label";
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+  render: (args) => (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="cb" {...args} />
+      <Label htmlFor="cb">Checkbox</Label>
+    </div>
+  ),
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: (args) => (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="cb" {...args} />
+      <Label htmlFor="cb">Disabled checkbox</Label>
+    </div>
+  ),
+};
