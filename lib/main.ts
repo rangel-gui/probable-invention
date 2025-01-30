2ba8da405f3441195f85823367f192d024d6dcaa
Gui Rangel <guirangel.310@gmail.com>
2025-01-29 21:46:14 -0300

    Export checkbox

--- lib/main.ts
+++ lib/main.ts
@@ -5,4 +5,5 @@ export type { ButtonProps } from "./components/ui/button";
 export { Input } from "./components/ui/input";
 export { Label } from "./components/ui/label";
 export { Card } from "./components/ui/card";
+export { Checkbox } from "./components/ui/checkbox";
 
