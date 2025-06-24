5105c29ede95be7967e4fbd8b9b946c94bf51f1e
Gui Rangel <guirangel.310@gmail.com>
2025-06-23 22:24:29 -0300

    Add style exports; Upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { peerDependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     dts({
-      rollupTypes: true,
+      insertTypesEntry: true,
       tsconfigPath: "./tsconfig.lib.json",
       include: ["lib"],
+      exclude: ["**/*.stories.ts", "**/*.test.tsx"],
     }),
-    libInjectCss(),
   ],
   resolve: {
     alias: {
@@ -24,10 +24,13 @@ export default defineConfig({
     copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
+      name: "probable-invention",
+      fileName: (format) => `probable-invention.${format}.js`,
+      formats: ["es", "umd"],
     },
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external: Object.keys(peerDependencies as Record<string, string>),
+      output: { globals: { react: "React", "react-dom": "ReactDOM" } },
     },
   },
 });
